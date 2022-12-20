e1765e2032d7b19bf5a1333c1a659b66e9fbff53
albertoBarl <alberto.barletta00@gmail.com>
2022-12-20 16:11:40 +0100

    added taskList

--- js/script.js
+++ js/script.js
@@ -2,6 +2,18 @@ const { createApp } = Vue;
 
 createApp({
   data() {
-    return {};
+    return {
+      refTask: 0,
+      taskList: [
+        { text: "Svegliarmi alle 5:30", done: false },
+        { text: "Bere acqua", done: false },
+        { text: "Sciaquarmi la faccia", done: false },
+        { text: "Radermi la barba", done: false },
+        { text: "Lavarmi i denti", done: false },
+      ],
+    };
+  },
+  methods: {
+    addTask(index) {},
   },
 }).mount("#appTodolist");
